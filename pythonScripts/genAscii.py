ba388c43fdd17c914ed3d98ac844be7ba47e3897
redfire <maxime.guilbert@neuf.fr>
2020-04-18 11:25:05 -0400

    #4 - Update python scripts to generate genAscii.ts and genText.ts - Add a command to remove generated files if they exist - Add writing encoding - Write the database module import at the beginning of generated files.

--- pythonScripts/genAscii.py
+++ pythonScripts/genAscii.py
@@ -11,14 +11,22 @@ for root, subFolders, files in os.walk(rootdir):
     for file in files:
         fullPath = os.path.join(root, file)
         tupleList.append((fullPath, os.path.splitext(fullPath)[0].replace(rootdir, "")))
-        
+
+
+# If a previous file exist, we delete it
+if os.path.isfile(finalFileName):
+    os.remove(finalFileName)
+
 # We open the output file
 outfile = open(finalFileName, mode='w', encoding="utf-8")
-        
+
+# We write the Database module import
+outfile.write("///<reference path=\"../main/modules/Database.ts\"/>\n")
+
 # We copy the content of each file in a big "genAscii.ts" file
 for tup in tupleList:
     # Open, read and close the input file
-    infile = open(tup[0], 'r')
+    infile = open(tup[0], 'r', encoding="utf-8")
     infileLines = infile.readlines()
     infile.close()
     # Check if there's an @author instruction or not
@@ -35,7 +43,7 @@ for tup in tupleList:
     if(author == True):
         height -= 1
     # Write the beginning of the Database.addAscii() call
-    outfile.write("Database.addAscii(\"" + tup[1] + "\", " + str(maxwidth-1) + ", " + str(height) + ", \n[\n")
+    outfile.write("Database.addAscii(\"" + tup[1].replace("\\", "/") + "\", " + str(maxwidth-1) + ", " + str(height) + ", \n[\n")
     # Write the ascii art itself to the function call
     for index, line in enumerate(infileLines):
         if(line.startswith("@author") == False or index > 0): # We don't write the line if it starts with @author and its the first line
--- pythonScripts/genText.py
+++ pythonScripts/genText.py
@@ -13,14 +13,21 @@ for root, subFolders, files in os.walk(rootdir):
         fullPath = os.path.join(root, file)
         if(os.path.splitext(fullPath)[0].replace(rootdir, "") != "cauldron/en"): # We don't want to use cauldron/en because the cauldron english text is contained in the book ascii art
             tupleList.append((fullPath, os.path.splitext(fullPath)[0].replace(rootdir, "")))
-        
+
+# If a previous file exist, we delete it
+if os.path.isfile(finalFileName):
+    os.remove(finalFileName)
+
 # We open the output file
 outfile = open(finalFileName, mode='w', encoding="utf-8")
+
+# We write the Database module import
+outfile.write("///<reference path=\"../main/modules/Database.ts\"/>\n")
 
 # We copy the content of each file in a big "genText.ts" file
 for tup in tupleList:
     # Open the input file
-    infile = open(tup[0], 'r')
+    infile = open(tup[0], 'r', encoding="utf-8")
     
     # Write the Database.addText() function calls
     for index, line in enumerate(infile.readlines()):
