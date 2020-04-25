529cf1a18e5db4d536a226536bfd9480321f667e
redfire <maxime.guilbert@neuf.fr>
2020-04-25 11:53:30 -0400

    #2 - WIP

---
+++ code/main/enums/VersionType.ts
@@ -0,0 +1,5 @@
+enum VersionType{
+    BEER,
+    CANDY,
+    HEALTHY
+}
---
+++ code/main/modules/Version.ts
@@ -0,0 +1,14 @@
+/**
+ * Contains methods to have elements which are which between versions.
+ */
+module Version {
+
+    export function getSingular(): string {
+        return " " + "candy";
+    }
+
+    export function getPlural(): string {
+        return " " + "candies";
+    }
+
+}
